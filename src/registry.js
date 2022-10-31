55162dc2e262bfac326fa4a4f8cbe5c476ac4816
Chinedu <chinedudimonyeka@gmail.com>
2022-10-31 13:41:20 +0100

    Build routes from registry

--- src/registry.js
+++ src/registry.js
@@ -1,20 +1,23 @@
 import HomeView from 'views/HomeView.vue'
 import AboutView from "views/AboutView.vue"
+
 export default [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-    bread_crumbs: ["home"],
-    icon: "mdi-view-dashboard",
-    title: "Home"
+    path: "/", // the unique path of the page
+    name: "home", // unique name of the page to be used in route object
+    component: HomeView, // The component to be displayed
+    bread_crumbs: ["home"], // the bread crumbs path
+    icon: "mdi-view-dashboard", // icon to be used in the sidebar menu
+    title: "Home", // The title of the page
+    show: true // bool to check whether to display page or not
   },
   {
     path: "/about",
     name: "about",
-    component: HomeView,
+    component: AboutView,
     bread_crumbs: ["about"],
     icon: "mdi-view-dashboard",
-    title: "About Us"
+    title: "About Us",
+    show: true
   },
 ]
