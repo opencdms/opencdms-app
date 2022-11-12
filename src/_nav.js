7dfa7ef841e376f29ecffa2731547714011785ad
Chinedu <chinedudimonyeka@gmail.com>
2022-11-12 22:12:04 +0100

    Adds page registry

--- src/_nav.js
+++ src/_nav.js
@@ -1,4 +1,6 @@
-export default [
+import registry from './registry.json'
+
+var pages = [
   {
     component: 'CNavItem',
     name: 'Dashboard',
@@ -6,3 +8,15 @@ export default [
     icon: 'cil-speedometer',
   },
 ]
+
+var processRegistry = (registry) => {
+    registry.map((page) => {
+         if (page.display == true){
+             delete page["display"]
+             pages.push(page)
+         }
+    })
+   return pages;
+}
+
+export default processRegistry(registry)
