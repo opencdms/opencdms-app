a73b4bb412347b3f6ddbced5730451908dc300f1
isedwards <isedwards@users.noreply.github.com>
2023-01-10 09:00:41 +0000

    Deploying to gh-pages from @ opencdms/opencdms-app@b4435cba006cd470186ca1b3f55c0249c3aa2b55 🚀

--- js/dashboard.100af252.js
+++ js/dashboard.100af252.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkopencdms_app"]=self["webpackChunkopencdms_app"]||[]).push([[966],{1160:function(n,e,a){a.r(e),a.d(e,{default:function(){return p}});var t=a(3396);const u=(0,t._)("h3",null,"This is the default view",-1),l=(0,t._)("p",null,"Default Content is not available. Please configure your application to start",-1),r=[u,l];function s(n,e,a,u,l,s){return(0,t.wg)(),(0,t.iD)("div",null,r)}var i={name:"Dashboard"},o=a(89);const c=(0,o.Z)(i,[["render",s]]);var p=c}}]);
-//# sourceMappingURL=dashboard.cb00e754.js.map
+//# sourceMappingURL=dashboard.100af252.js.map
