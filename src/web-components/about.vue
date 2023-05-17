e74b2c942b528289de958cea0b8bbe9948d09f20
david-i-berry <dberry@wmo.int>
2023-05-17 13:11:25 +0200

    Caveat on access control added to home page / about.

--- src/web-components/about.vue
+++ src/web-components/about.vue
@@ -9,8 +9,17 @@
     <a href="https://openclimate.net/">https://openclimate.net/</a>. The web application has been built using open
     software and standards, the source code can be found in the project repository on GitHub:
     <a href="https://www.github.com/opencdms">https://www.github.com/opencdms</a>.
-
-
+    </v-card-text>
+  </v-card>
+  <v-card>
+    <v-card-title>Access control and authentication</v-card-title>
+    <v-card-text>Access control and authentication has not been implemented in the
+    current version of the software but will be implemented as part of the first release.
+    The access control will be implemented at both the user and role level, with some users or roles
+    only able to view data, other users able to edit a subset of the data and a small number with full
+    access. Where the OpenCDMS interacts with other systems, for exampling publishing to the WMO Information
+    System or updating records in OSCAR/Surface, the roles will be assigned to the respective national focal points.
+    Only those users with appropriate roles will be able to edit and publish the relevant metadata.
     </v-card-text>
   </v-card>
   <v-card>
@@ -36,8 +45,7 @@
     <v-list-item style=" font-size: 14px;">
       <v-list-item-title>Watershed</v-list-item-title>
       Watershed boundaries come from the <a href="https://geohub.lio.gov.on.ca/maps/53a1c537b320404087c54ef09700a7db/explore?location=41.945997%2C-82.339420%2C4.94">
-      Ontario Watershed Boundaries (OWB) collection></a>. These have been accessed under the <a href="https://www.ontario.ca/page/open-government-licence-ontario">Open Government Licence – Ontario</a> licence.
-
+      Ontario Watershed Boundaries (OWB) collection</a>. These have been accessed under the <a href="https://www.ontario.ca/page/open-government-licence-ontario">Open Government Licence – Ontario</a> licence.
     </v-list-item>
     </v-list>
     </v-card-item>
